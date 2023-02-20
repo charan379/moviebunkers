ff4bea6d2d17f37bf686ac0e6b7a277158aeb418
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-20 16:10:05 +0530

    404 endpoint added

--- src/app/routes/api.router.js
+++ src/app/routes/api.router.js
@@ -3,8 +3,17 @@ const router = express.Router();
 
 const usersRouter = require("./users.router");
 const titlesRouter = require("./titles.router");
+const ErrorResponse = require("../utils/ErrorResponse");
 
 router.use("/users", usersRouter);
 router.use("/titles", titlesRouter);
+
+/**
+ * unknown route
+ */
+router.use("/*", function (req, res, next) {
+  res.status(404).json(ErrorResponse("Requested Endpoint doesn't exit"));
+});
+
 
 module.exports = router;
