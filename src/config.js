879a893a477feba8d59c475daf9e78cb538f806e
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-23 21:26:19 +0530

    port added

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 exports.Config = Object.freeze({
+  PORT: process.env.PORT || 3000,
   MongoDB_SERVER_STRING: process.env.MongoDB_SERVER_STRING,
   HTTPS: JSON.parse(process.env.HTTPS || false),
   COOKIE_SECRET: process.env.COOKIE_SECRET,
