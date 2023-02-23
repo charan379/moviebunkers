adc370bfd98db732b806c09e5ed0e7727d644fd0
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-23 16:40:44 +0530

    use config and invalid signature

--- src/app/utils/jwt.token.js
+++ src/app/utils/jwt.token.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const { Config } = require("../../config");
 const { InvalidToken, ExpiredToken } = require("../errors/AuthorizationErrors");
 const { AuthorizationException } = require("./Exceptions");
 
@@ -16,7 +17,7 @@ exports.generateJwtToken = async (userObj) => {
     /**
      * create new jwt token and store in token variable
      */
-    token = jwt.sign({ ...userObj }, "secretkeyappearshere", {
+    token = jwt.sign({ ...userObj }, Config.JWT_SECRET, {
       expiresIn: "1d",
     });
     /**
@@ -49,12 +50,20 @@ exports.verifyJwtToken = async (token) => {
     /**
      * verify jwt token and store it in deCodedToken variable
      */
-    deCodedToken = jwt.verify(token, "secretkeyappearshere");
+    deCodedToken = jwt.verify(token, Config.JWT_SECRET);
 
     /**
      * catch if any error is thrown by above process
      */
   } catch (error) {
+    console.log(error.message)
+    /**
+     * if thrown error is " invalid signature "
+     */
+    if (error.message === "invalid signature") {
+      throw new AuthorizationException(InvalidToken(error.message));
+    }
+
     /**
      * if thrown error is " invalid token "
      */
@@ -68,7 +77,7 @@ exports.verifyJwtToken = async (token) => {
     if (error.message === "jwt expired") {
       throw new AuthorizationException(ExpiredToken(error.message));
     }
-
+    
     /**
      * If any other error is thrown 
      */
