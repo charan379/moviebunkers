e29aac4469204650546d4672f90162bb211b3ab0
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-01 16:10:08 +0530

    user joi schema updated

--- src/app/joiSchemas/user.joi.schemas.ts
+++ src/app/joiSchemas/user.joi.schemas.ts
@@ -3,7 +3,7 @@ import UserRoles from "@constants/user.roles.enum";
 import UserStatus from "@constants/user.status.enum";
 
 export const userNameSchema: Joi.StringSchema = Joi.string().min(5).max(16).example("user00001");
-export const emailSchema = Joi.string().email().example("user00001@gmail.com");
+export const emailSchema: Joi.StringSchema = Joi.string().email().example("user00001@gmail.com");
 
 export const passwordSchema: Joi.StringSchema = Joi.string()
   .min(8)
@@ -14,13 +14,19 @@ export const userRegistrationSchema: Joi.ObjectSchema<any> = Joi.object({
   userName: userNameSchema.required(),
   email: emailSchema.required(),
   password: passwordSchema.required(),
+});
+
+export const newUserSchema: Joi.ObjectSchema<any> = Joi.object({
+  userName: userNameSchema.required(),
+  email: emailSchema.required(),
+  password: passwordSchema.required(),
   role: Joi.string().valid(UserRoles.USER),
   status: Joi.string().valid(UserStatus.ACTIVE),
 });
 
-export const findAllUserQuerySchema : Joi.ObjectSchema<any> = Joi.object({
-  userName: userNameSchema,
-  email: emailSchema,
+export const findAllUserQuerySchema: Joi.ObjectSchema<any> = Joi.object({
+  userName: Joi.string().allow(null).allow(""),
+  email: Joi.string().allow(null).allow(""),
   role: Joi.string().valid(...Object.values(UserRoles)),
   status: Joi.string().valid(...Object.values(UserStatus)),
   minimal: Joi.boolean(),
@@ -28,3 +34,8 @@ export const findAllUserQuerySchema : Joi.ObjectSchema<any> = Joi.object({
   limit: Joi.number().integer().example(5),
   sort_by: Joi.string().example("role.desc"),
 })
+
+export const userUpdateSchema: Joi.ObjectSchema = Joi.object({
+  status: Joi.string().valid(...Object.values(UserStatus)),
+  role: Joi.string().valid(...[UserRoles.USER, UserRoles.MODERATOR]),
+});
