9f5f829fb1edbd62b36af1dcc622fb278f53756e
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-01 13:59:08 +0530

    Mongo Sort_order builder

---
+++ src/app/utils/mongo.sort.builder.ts
@@ -0,0 +1,21 @@
+
+
+export default async function MongoSortBuilder(sort_by: string) {
+
+    const elementRegex: RegExp = /(^[a-zA-Z0-9]\w+\.(desc|asc))/;
+
+    let sortElementsObject: object = {};
+
+    const sortElementsArray : string[] = sort_by.split(",");
+
+    for (let index: number = 0; index < sortElementsArray.length; index++) {
+        const element = sortElementsArray[index];
+
+        if (element.match(elementRegex)) {
+            const [key, value] = element.split(".");
+            sortElementsObject = { ...sortElementsObject, [key]: value };
+        }
+    }
+
+    return sortElementsObject;
+}
