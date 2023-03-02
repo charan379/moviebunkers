c1244170ac8de5481116e00dd06716bd1c9f8929
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-02 18:55:32 +0530

    mojor tweaks

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,15 @@
 import "reflect-metadata";
-import createHttpError, { HttpError } from "http-errors";
-import debugLogger from "debug";
-import express, { application, Application, NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import express, { Application } from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
-import logger from "morgan";
 import stylus from "stylus";
-import MoviebunkersException from "@exceptions/moviebunkers.exception";
-import ErrorResponse from "./app/utils/ErrorResponse";
 import webRouter from '@routes/pug.routes'
 import apiRouter from '@routes/api.routes'
-
-const debug = debugLogger("moviebunkers:[app.ts");
+import ErrorHandler from "@middlewares/errorHandler.middleware";
+import { ConsoleLogger } from "@middlewares/logger.middleware";
 
 const app: Application = express();
-
 
 // set app environment
 app.set("env", "development");
@@ -22,47 +17,25 @@ app.set("env", "development");
 // view engine setup
 app.set("views", path.join(__dirname, "/app/views"));
 app.set("view engine", "pug");
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("cookieSecretHere"));
 app.use(stylus.middleware(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../public")));
 
+app.use(ConsoleLogger);
 
 app.use('/', webRouter);
 app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
+  // res.status(404).json({message: "Route not found"})
   next(createHttpError(404));
 });
 
 // error handler ( default handler provided by Express if we are not handling error then it will catch errors by default)
-app.use(function (
-  err: HttpError | Error | MoviebunkersException,
-  req: Request | any,
-  res: Response | any,
-  next: NextFunction
-) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  debug(err);
-
-  // render the error page
-  if (err instanceof HttpError) {
-    res.status(err?.status || 500);
-    res.json(ErrorResponse(err))
-  } else if (err instanceof MoviebunkersException) {
-    res.status(err?.status ?? 500);
-    res.json(ErrorResponse(err));
-  } else {
-    res.status(500)
-    res.json(ErrorResponse(err))
-  }
-
-});
+app.use(ErrorHandler);
 
 export default app;
