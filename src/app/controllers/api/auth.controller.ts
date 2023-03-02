797cf8ef83c7b2c0c4ff463e64402a3533067d8b
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-02 18:55:56 +0530

    auth controller added

---
+++ src/app/controllers/api/auth.controller.ts
@@ -0,0 +1,66 @@
+import { LoginDTO } from "@dto/user.dto";
+import { lgoinSchema } from "@joiSchemas/user.joi.schemas";
+import { AuthService } from "@service/auth.service";
+import JoiValidator from "@utils/joi.validator";
+import { CookieOptions, NextFunction, Request, Response, Router } from "express";
+import { Inject, Service } from "typedi";
+
+
+@Service()
+class AuthController {
+
+    private authService: AuthService;
+
+    public router: Router = Router();
+    constructor(@Inject() authService: AuthService) {
+        this.authService = authService;
+
+        //login
+        this.router.post("/login", this.login.bind(this));
+
+        //logout
+        this.router.get("/logout", this.logout.bind(this));
+    }
+
+
+    private async login(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const validLoginDTO: LoginDTO = await JoiValidator(lgoinSchema, req?.body, { allowUnknown: false, stripUnknown: true, abortEarly: false });
+
+            const token: string = await this.authService.login(validLoginDTO);
+
+            res.cookie("auth", `Bearer ${token}`, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+                // secure: Config.HTTPS,
+                signed: true,
+                overwrite: true,
+                sameSite: false,
+            } as CookieOptions)
+            .status(200)
+            .json({ message: "Successfully Logged In" });
+
+        } catch (error) {
+
+            next(error)
+        }
+    }
+
+
+    private async logout(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+
+            res.clearCookie("auth")
+            .status(200)
+            .json({ message: "Successfully Logged Out" })
+
+        } catch (error) {
+
+            next(error)
+
+        }
+    }
+
+}
+
+export default AuthController;
