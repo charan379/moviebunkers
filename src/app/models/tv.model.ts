b0b037ac5cb95fc85998b76c2226d58f1611a1bb
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-03 01:05:18 +0530

    tv model updated

--- src/app/models/tv.model.ts
+++ src/app/models/tv.model.ts
@@ -1,61 +1,21 @@
 import mongoose, { Model, Schema } from "mongoose";
+import authorExtendableSchema from "./extendable.schemas/author.extendable.schema";
+import baseTitleExtendableSchema from "./extendable.schemas/base.title.extendable.schema";
+import tvExtendableSchema from "./extendable.schemas/tv.extendable.schema";
 import ITv from "./interfaces/tv.interface";
-import { movieSchema } from "./movie.model";
 
 
-const tvSchema: Schema<ITv> = new Schema<ITv>({
-    ...movieSchema,
 
-    // in_production
-    in_production: {
-        type: Boolean,
-    },
-    // created_by
-    created_by: {
-        type: [String],
-    },
-    // last_aired_date
-    last_aired_date: {
-        type: Date,
-    },
-    // last_episode_aired
-    last_episode_aired: {
-        type: Object,
-        _id: false,
-    },
-    // next_episode_to_air
-    next_episode_to_air: {
-        type: Object,
-        _id: false,
-    },
-    // networks
-    networks: {
-        type: [String],
-        _id: false,
-    },
-
-    // number_of_seasons
-    number_of_seasons: {
-        type: Number,
-    },
-    // number_of_episodes
-    number_of_episodes: {
-        type: Number,
-    },
-    // seasons
-    seasons: {
-        type: [Object],
-        _id: false,
-    },
-},
-    // {
-    //     timestamps: {
-    //         createdAt: true,
-    //         updatedAt: true,
-    //     },
-    //     collection: "titles",
-    // }
-);
+const tvSchema: Schema<ITv> = new Schema<ITv>(
+    {},
+    {
+        timestamps: {
+            createdAt: true,
+            updatedAt: true,
+        },
+        collection: "titles",
+    }
+).add(baseTitleExtendableSchema.add(tvExtendableSchema.add(authorExtendableSchema)));
 
 
 const TvModel: Model<ITv> = mongoose.model<ITv>("tv", tvSchema);
