33e95326805ce34b2a3732fc411517e09c981559
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-02 18:55:38 +0530

    deleted

--- src/logger.ts
+++
@@ -1,8 +0,0 @@
-import debug from "debug";
-import { NextFunction } from "express";
-
-export function loggerCustom (req: any, res: any, next: NextFunction) {
-    const logger = debug('moviebunkers:[logger]')
-    logger('logger middle ware working')
-    next();
-}
