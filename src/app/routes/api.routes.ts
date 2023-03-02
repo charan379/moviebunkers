018ce3c39d6d4d3a73d0596fc342ea5097ecb260
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-02 19:04:41 +0530

    auth controller added

--- src/app/routes/api.routes.ts
+++ src/app/routes/api.routes.ts
@@ -1,3 +1,4 @@
+import AuthController from "@controllers/api/auth.controller";
 import { UserController } from "@controllers/api/users.controller";
 import { Router } from "express";
 import Container from "typedi";
@@ -8,5 +9,11 @@ const router = Router();
 const userController = Container.get(UserController);
 
 router.use("/users", userController.router);
+
+
+const authController = Container.get(AuthController);
+
+router.use("/auth", authController.router);
+
 
 export default router;
