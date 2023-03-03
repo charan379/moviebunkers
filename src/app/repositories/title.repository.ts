baaf9dc3a05a88e916867356acef65e13d3f3bf7
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-03 16:17:04 +0530

    title repository added

---
+++ src/app/repositories/title.repository.ts
@@ -0,0 +1,66 @@
+import PageDTO from "@dto/page.dto";
+import TitleDTO from "@dto/title.dto";
+import ITitle from "@models/interfaces/title.interface";
+import TitleModel from "@models/title.model";
+import { Model, ProjectionFields } from "mongoose";
+import { Service } from "typedi";
+import { FindAllQuery } from "./interfaces/custom.types.interfaces";
+import ITitleRepository from "./interfaces/title.repository.interface";
+
+@Service()
+class TitleRepository implements ITitleRepository {
+
+    private titleModel: Model<ITitle>;
+
+    constructor() {
+        this.titleModel = TitleModel;
+    }
+
+    findById(id: string, projection: ProjectionFields<ITitle>= {__v: 0}): Promise<ITitle | null> {
+        return this.titleModel.findById(id, projection).lean().exec();
+    }
+
+    findByTmdbId(tmdbId: number, projection: ProjectionFields<ITitle>= {__v: 0}): Promise<ITitle | null> {
+        return this.titleModel.findOne({tmdb_id: tmdbId},projection).lean().exec();
+    }
+
+    findByImdbId(imdbId: string, projection: ProjectionFields<ITitle>= {__v: 0}): Promise<ITitle | null> {
+        return this.titleModel.findOne({imdb_id: imdbId}, projection).lean().exec();
+    }
+
+    /**
+     * 
+     * @param param0 FindAllQuery
+     * @param projection ProjectionFields<ITitle>
+     * @returns 
+     */
+    async findAll({ query, sort, limit, page }: FindAllQuery, projection: ProjectionFields<ITitle>= {__v: 0}): Promise<PageDTO> {
+
+
+        const total_results = await this.titleModel.find({ ...query }).countDocuments().lean().exec();
+
+        const titlesList: ITitle[] = await this.titleModel.find({ ...query }, projection)
+            .limit(limit * 1)
+            .skip((page - 1) * limit)
+            .sort({ ...sort })
+            .lean()
+            .exec();
+
+        const titleDTOs: TitleDTO[] = titlesList.map(iTitle => {
+            return iTitle as TitleDTO;
+        })
+
+        const result: PageDTO = {
+            page: page,
+            total_pages: Math.ceil(total_results / limit),
+            total_results: total_results,
+            sort_order: sort,
+            list: titleDTOs,
+        }
+
+        return result;
+    }
+
+}
+
+export default TitleRepository;
