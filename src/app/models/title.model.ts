4eb9f2224369a76acca83b9ad479b7e70c20adb1
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-03 16:13:54 +0530

    title model added

--- src/app/models/title.model.ts
+++ src/app/models/title.model.ts
@@ -1,15 +1,14 @@
 import mongoose, { Model, Schema } from "mongoose";
-import IMovie from "./interfaces/movie.interface";
-import ITv from "./interfaces/tv.interface";
+import ITitle from "./interfaces/title.interface";
 
 
-const titleSchema: Schema<IMovie | ITv> = new Schema<IMovie | ITv>(
+const titleSchema: Schema<ITitle> = new Schema<ITitle>(
     {},
     {
         collection: "titles",
     }
 );
 
-const TitleModel: Model<IMovie | ITv> = mongoose.model<IMovie | ITv>("title", titleSchema);
+const TitleModel: Model<ITitle> = mongoose.model<ITitle>("title", titleSchema);
 
 export default TitleModel;
