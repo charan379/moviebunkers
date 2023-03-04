6d03f9fca3b75af902000f4ff8c38ba267dc91ae
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-04 11:06:36 +0530

    TitleDTO added

--- src/app/dto/title.dto.ts
+++ src/app/dto/title.dto.ts
@@ -1,48 +1,12 @@
-import TitleType from "@constants/titile.types.enum";
-import TitleSource from "@constants/title.souces.enum";
-import CastDTO from "./cast.dto";
-import EpisodeDTO from "./episode.dto";
-import LanguageDTO from "./language.dto";
-import SeasonDTO from "./season.dto";
+import MovieDTO from "./movie.dto";
+import TvDTO from "./Tv.dto";
 
 
-interface TitleDTO {
-    title_type: TitleType;
-    source: TitleSource;
-    imdb_id?: string;
-    tmdb_id?: number;
-    title: string;
-    original_title: string;
-    original_language: LanguageDTO;
-    languages?: LanguageDTO[];
-    tagline?: string;
-    poster_path?: string;
-    year?: number;
-    runtime?: number;
-    ratting?: number;
-    age_ratting?: number;
-    genres: string[];
-    overview?: string;
-    production_companies?: string[];
-    production_countries?: string[];
-    status: string;
-    release_date?: Date;
-    providers?: string[];
-    directors?: string[];
-    cast?: CastDTO[];
-    in_production?: boolean;
-    created_by?: string[];
-    last_aired_date?: Date;
-    last_episode_aired?: EpisodeDTO;
-    next_episode_to_air?: EpisodeDTO;
-    networks?: string[];
-    number_of_seasons?: number;
-    number_of_episodes?: number;
-    seasons?: SeasonDTO[];
-    added_by: string;
-    last_modified_by?: string;
-    createdAt: Date;
-    updatedAt: Date;
+interface TitleDTO extends MovieDTO, TvDTO, TitleAuthorDTO {
+
+
 }
+
+
 
 export default TitleDTO;
