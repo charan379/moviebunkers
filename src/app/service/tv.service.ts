468c176cf0833692f4c7db254884822f69e8b384
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-04 17:24:00 +0530

    createTv method implemented

--- src/app/service/tv.service.ts
+++ src/app/service/tv.service.ts
@@ -1,12 +1,28 @@
 import TvDTO from "@dto/Tv.dto";
-import { Service } from "typedi";
+import tvSchema from "@joiSchemas/tv.joi.schema";
+import ITv from "@models/interfaces/tv.interface";
+import TvRepository from "@repositories/tv.repository";
+import JoiValidator from "@utils/joi.validator";
+import { Inject, Service } from "typedi";
 import ITvService from "./interfaces/tv.service.interface";
 
 
 @Service()
 class TvService implements ITvService {
-    createTv(tvDTO: Partial<TvDTO>): Promise<TvDTO> {
-        throw new Error("Method not implemented.");
+
+    private tvRepository: TvRepository;
+
+    constructor(@Inject() tvRepository: TvRepository){
+        this.tvRepository = tvRepository;
+    }
+
+    async createTv(tvDTO: Partial<TvDTO>): Promise<TvDTO> {
+        
+        const validTV: TvDTO = await JoiValidator(tvSchema, tvDTO, {abortEarly:false, stripUnknown: true, allowUnknown: false});
+        
+        const newMovie = await this.tvRepository.create(validTV as Partial<ITv>) as TvDTO;
+
+        return newMovie;
     }
 }
 
