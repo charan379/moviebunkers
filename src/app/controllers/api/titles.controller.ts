4c85b7fcdee478c81d3702e9d4abe2de0afc59e5
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-04 17:25:01 +0530

    createTitle implemented

--- src/app/controllers/api/titles.controller.ts
+++ src/app/controllers/api/titles.controller.ts
@@ -1,5 +1,10 @@
-import { NextFunction, Request, Response, Router, RouterOptions } from "express";
-import { Service } from "typedi";
+import TitleDTO from "@dto/title.dto";
+import baseTitleSchema from "@joiSchemas/base.joi.title.schema";
+import { ObjectIdSchema } from "@joiSchemas/common.joi.schemas";
+import TitleService from "@service/title.service";
+import JoiValidator from "@utils/joi.validator";
+import { NextFunction, Request, Response, Router } from "express";
+import { Inject, Service } from "typedi";
 
 /**
  * @Controller("/titles") => TitleController.class
@@ -8,9 +13,11 @@ import { Service } from "typedi";
 class TitleController {
 
     public router: Router;
-    private titleService: any;
+    private titleService: TitleService;
 
-    constructor() {
+    constructor(@Inject() titleService: TitleService) {
+        this.titleService = titleService;
+
         this.router = Router();
 
         //get
@@ -46,10 +53,14 @@ class TitleController {
      * @param next 
      */
     private async getTitleById(req: Request, res: Response, next: NextFunction) {
-        
+
         try {
 
-            throw new Error("method not implemented")
+            const validId = await JoiValidator(ObjectIdSchema, req?.params?.id, {abortEarly: false, allowUnknown: false, stripUnknown: true})
+            
+            const titileDTO: TitleDTO = await this.titleService.getTitleById(validId);
+
+            res.status(200).json(titileDTO)
 
         } catch (error) {
 
@@ -64,10 +75,13 @@ class TitleController {
      * @param next 
      */
     private async createTitle(req: Request, res: Response, next: NextFunction) {
-       
-        try {
 
-            throw new Error("method not implemented")
+        try {
+            const titleDTO: Partial<TitleDTO> = await JoiValidator(baseTitleSchema, req.body, { abortEarly: false, allowUnknown: true, stripUnknown: false });
+
+            const newTitle: TitleDTO = await this.titleService.createTitle(titleDTO);
+            
+            res.status(201).json({message:"New Title Added Successfully",new_title_id: newTitle._id})
 
         } catch (error) {
 
