73b9889a4d1d21b48a3f81379709616b63b6dcca
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-04 17:27:39 +0530

    createMovie implemented

--- src/app/service/movie.service.ts
+++ src/app/service/movie.service.ts
@@ -1,11 +1,28 @@
-import { Service } from "typedi";
+import MovieDTO from "@dto/movie.dto";
+import movieSchema from "@joiSchemas/movie.joi.schema";
+import IMovie from "@models/interfaces/movie.interface";
+import MovieRepository from "@repositories/movie.repository";
+import JoiValidator from "@utils/joi.validator";
+import { Inject, Service } from "typedi";
 import IMovieService from "./interfaces/movie.service.interface";
-
 
 @Service()
 class MovieService implements IMovieService {
-    createMovie(movieDTO: Partial<MovieDTO>): Promise<MovieDTO> {
-        throw new Error("Method not implemented.");
+    
+    private movieRepository: MovieRepository;
+
+    constructor (@Inject() movieRepository: MovieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+
+    async createMovie(movieDTO: Partial<MovieDTO>): Promise<MovieDTO> {
+        
+        const validMovie: MovieDTO = await JoiValidator(movieSchema, movieDTO, {abortEarly:false, stripUnknown: true, allowUnknown: false});
+        
+        const newMovie = await this.movieRepository.create(validMovie as Partial<IMovie>) as MovieDTO;
+
+        return newMovie;
     }
 }
 
