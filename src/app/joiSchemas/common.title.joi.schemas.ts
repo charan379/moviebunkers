aeeb1c63f6fe014b27d7e9b7578abd4a08062d38
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-04 17:25:57 +0530

    allow nulls and ""

--- src/app/joiSchemas/common.title.joi.schemas.ts
+++ src/app/joiSchemas/common.title.joi.schemas.ts
@@ -14,31 +14,35 @@ export const castSchema: Joi.ObjectSchema = Joi.object({
 });
 
 export const episodeSchema: Joi.ObjectSchema = Joi.object({
-    air_date: Joi.date().example("2013-04-28"),
+    air_date: Joi.date().example("2013-04-28").allow(null).allow(""),
     episode_number: Joi.number().integer().required().example(5),
     season_number: Joi.number().integer().required().example(3),
     name: Joi.string().required().example("Kissed by Fire"),
     overview: Joi.string().example(
         "The Hound is judged by the gods. Jaime is judged by men. Jon proves himself. Robb is betrayed. Tyrion learns the cost of weddings."
-    ),
+    ).allow(null).allow(""),
     runtime: Joi.number().integer().example(57),
     still_path: Joi.string().example(
         "https://image.tmdb.org/t/p/w300/41CekEZyGvNLTJIJy7BqFDTitcC.jpg"
-    ),
-    directors: Joi.array().items(Joi.string().example("Alex Graves")),
+    ).allow(null).allow(""),
+    directors: Joi.array().items(Joi.string().example("Alex Graves")).allow(null).allow(""),
 });
 
 export const seasonSchema: Joi.ObjectSchema = Joi.object({
-    air_date: Joi.date().example("2013-03-31"),
+    air_date: Joi.date().example("2013-03-31").allow(null).allow(""),
     season_number: Joi.number().required().example(3),
     episode_count: Joi.number().required().example("10"),
     name: Joi.string().required().example("Season 3"),
     overview: Joi.string()
         .example(
             "Daenerys Targaryen--reunited with her dragons--attempts to raise an army in her quest for the Iron Throne."
-        ),
+        )
+        .allow(null)
+        .allow(""),
     poster_path: Joi.string()
-        .example("https://image.tmdb.org/t/p/w300/5MkZjRnCKiIGn3bkXrXfndEzqOU.jpg"),
+        .example("https://image.tmdb.org/t/p/w300/5MkZjRnCKiIGn3bkXrXfndEzqOU.jpg")
+        .allow(null)
+        .allow(""),
 });
 
 export const titleAuthorSchema: Joi.ObjectSchema = Joi.object({
@@ -50,7 +54,7 @@ export const titleAuthorSchema: Joi.ObjectSchema = Joi.object({
 });
 
 export const newTitleInitialCheckSchema: Joi.ObjectSchema = Joi.object({
-    
+
     title_type: Joi.string()
         .valid(...Object.values(TitleType))
         .required(),
