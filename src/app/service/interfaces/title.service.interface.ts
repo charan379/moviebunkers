ad25f980b85fb8712490cccc9267ff95f90bf91e
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-05 16:58:35 +0530

    getAllTitles added

--- src/app/service/interfaces/title.service.interface.ts
+++ src/app/service/interfaces/title.service.interface.ts
@@ -1,11 +1,12 @@
-import TitleDTO from "@dto/title.dto";
+import PageDTO from "@dto/page.dto";
+import TitleDTO, { FindAllTitlesQueryDTO } from "@dto/title.dto";
 
 
 interface ITitleService {
 
     createTitle(titileDTO: Partial<TitleDTO>): Promise<TitleDTO>;
     getTitleById(id: string): Promise<TitleDTO>;
-
+    getAllTitles(queryDTO: FindAllTitlesQueryDTO): Promise<PageDTO>;
 }
 
 export default ITitleService;
