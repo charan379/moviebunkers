72de384e062b9d990bf63a47db2215297713f3f9
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-05 16:57:16 +0530

    FindAllTitlesQueryDTO added

--- src/app/dto/title.dto.ts
+++ src/app/dto/title.dto.ts
@@ -1,3 +1,5 @@
+import TitleType from "@constants/titile.types.enum";
+import LanguageDTO from "./language.dto";
 import MovieDTO from "./movie.dto";
 import TvDTO from "./Tv.dto";
 
@@ -6,6 +8,16 @@ interface TitleDTO extends MovieDTO, TvDTO, TitleAuthorDTO {
     _id?: string;
 }
 
-
-
 export default TitleDTO;
+
+export interface FindAllTitlesQueryDTO {
+    search: string;
+    title_type: TitleType;
+    genre: string;
+    year: number;
+    language: string;
+    minimal?: boolean,
+    page?: number,
+    sort_by?: string,
+    limit?: number
+}
