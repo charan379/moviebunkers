052c2faf9a9e09c3675ef49402d0732a98e3e7c0
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-16 22:47:35 +0530

    `update` age ratting updated

--- src/app/constants/age.rattings.ts
+++ src/app/constants/age.rattings.ts
@@ -1,19 +1,20 @@
 const AgeRattings = {
     IN: [ // India
-        { certification: "U", age: 0 },
-        { certification: "UA", age: 12 },
-        { certification: "A", age: 18 },
-        { certification: "S", age: 22 },
-        { certification: "MB-26", age: 26 },
+        { certification: "U", age: [0, 11] },
+        { certification: "UA", age: [12, 17] },
+        { certification: "A", age: [18, 21] },
+        { certification: "S", age: [21, 26] },
+        { certification: "MB-26", age: [21, 26] },
     ],
 
     DE: [ // germany
-        { certification: "0", age: 0 },
-        { certification: "6", age: 6 },
-        { certification: "12", age: 12 },
-        { certification: "16", age: 16 },
-        { certification: "18", age: 18 },
-        { certification: "MB-26", age: 26 },
+        { certification: "0", age: [0, 5] },
+        { certification: "6", age: [6, 11] },
+        { certification: "12", age: [12, 15] },
+        { certification: "16", age: [16, 17] },
+        { certification: "18", age: [18, 21] },
+        { certification: "Educational", age: [22, 26] },
+        { certification: "MB-26", age: [21, 26] },
     ]
 };
 
