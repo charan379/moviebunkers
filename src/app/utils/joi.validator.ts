a05aec1586dd16f639b6009ac10cfa8f6ab1938f
charan379 <37008444+charan379@users.noreply.github.com>
2023-03-18 17:00:43 +0530

    `update`: accept anything to validate, and log calling funtion in stack trace

--- src/app/utils/joi.validator.ts
+++ src/app/utils/joi.validator.ts
@@ -2,13 +2,13 @@ import Joi from "joi";
 import HttpCodes from "@constants/http.codes.enum";
 import ValidationException from "@exceptions/validation.expception";
 
-async function JoiValidator(joiSchema: Joi.ObjectSchema<any> | Joi.AnySchema , requestBody: Object, options: Joi.ValidationOptions){
+async function JoiValidator(joiSchema: Joi.ObjectSchema<any> | Joi.AnySchema, requestBody: Object | any, options: Joi.ValidationOptions, sourceFunction: string = "Not provided") {
 
 
-    const {value: DTO, error: errors} = await joiSchema.validate(requestBody, options);
+    const { value: DTO, error: errors } = joiSchema.validate(requestBody, options);
 
-    if (errors){
-        throw new ValidationException("Request Validation Failed", HttpCodes.BAD_REQUEST, errors.message, `@JoiValidator.function(): requestBody: ${JSON.stringify(requestBody)},  ${errors.stack}` )
+    if (errors) {
+        throw new ValidationException("Request Validation Failed", HttpCodes.BAD_REQUEST, errors.message, `@JoiValidator.function(): requestBody: ${JSON.stringify(requestBody)} sourceFunction: ${sourceFunction},  ${errors.stack}`)
     }
 
     return DTO;
