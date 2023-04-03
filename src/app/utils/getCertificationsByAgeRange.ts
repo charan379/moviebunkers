023ff1adf0ed73c21ed2c7bd18a260a080a7dc29
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-03 19:25:35 +0530

    `code-cleaning` getCertificationsByAgeRange

--- src/app/utils/getCertificationsByAgeRange.ts
+++ src/app/utils/getCertificationsByAgeRange.ts
@@ -1,11 +1,28 @@
-import AgeRattings from "@constants/age.rattings";
-import Country from "@constants/country.enum";
+import AgeRatings from '@constants/age.rattings';
+import Country from '@constants/country.enum';
 
+/**
+ * Returns an array of certifications within the given age range and country.
+ *
+ * @param {number} lowerAgeLimit - The lower age limit of the certification.
+ * @param {number} upperAgeLimit - The upper age limit of the certification.
+ * @param {Country} country - The country for which certifications are required.
+ * @returns {Promise<string[]>} - An array of certifications that match the given criteria.
+ * @throws {Error} - If age ratings for the given country are not found.
+ */
+async function getCertificationsByAgeRange(lowerAgeLimit: number, upperAgeLimit: number, country: Country): Promise<string[]> {
+  // Get the age ratings for the given country.
+  const ageRatings = AgeRatings[country];
 
-async function getCertificationsByAgeRange(lowerAgeLimit: Number, upperAgeLimit: Number, country: Country) {
-  const certifications = AgeRattings?.[country].map(certification => {
-    if (certification.age[0] <= upperAgeLimit && certification.age[1] >= lowerAgeLimit) return certification.certification
-  }).filter(Boolean)
+  // If no age ratings are found, throw an error.
+  if (!ageRatings) {
+    throw new Error(`Age ratings for country ${country} not found`);
+  }
+
+  // Filter the age ratings that match the given age range, and extract the certification values.
+  const certifications = ageRatings
+    .filter((rating: any) => rating.age[0] <= upperAgeLimit && rating.age[1] >= lowerAgeLimit)
+    .map((rating: any) => rating.certification);
 
   return certifications;
 }
