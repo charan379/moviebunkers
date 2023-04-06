4245da5d2eaf6d59485503ebfd289a34cf23eefe
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-06 17:07:15 +0530

    `new` links model

---
+++ src/app/models/links.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Model, Schema } from "mongoose";
+import ILink from "./interfaces/ILinks";
+import ContentType from "@constants/contentTypes.enum";
+import LinkType from "@constants/linkTypes.enum";
+import ContentQuality from "@constants/contenetQuality.enum";
+
+
+const linkSchema: Schema<ILink> = new Schema<ILink>(
+    {
+        parentId: {
+            type: Schema.Types.ObjectId,
+        },
+        contentType: {
+            type: String,
+            enum: [...Object.values(ContentType)],
+            required: [true, "content type required"],
+        },
+        linkType: {
+            type: String,
+            enum: [...Object.values(LinkType)],
+            required: [true, 'link type is required']
+        },
+        quality: {
+            type: [String],
+            enum: [...Object.values(ContentQuality)],
+            required: [true, 'atleast one quality type is required']
+        },
+        link: {
+            type: String,
+            required: [true, 'link is required']
+        }
+    },
+    {
+        timestamps: {
+            createdAt: true,
+            updatedAt: true,
+        },
+        collection: "links"
+    }
+);
+
+const LinkModel: Model<ILink> = mongoose.model<ILink>("Link", linkSchema);
+
+export default LinkModel;
