4f20ec7f0e33e6823d159c3e464642af8c0ef814
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-06 17:05:44 +0530

    `new` quality type enum

---
+++ src/app/constants/contenetQuality.enum.ts
@@ -0,0 +1,13 @@
+
+
+enum ContentQuality {
+    VIDEO_HD = 'HD',
+    VIDEO_UHD = 'Ultra HD',
+    VIDEO_320 = '320P',
+    VIDEO_480 = '480P',
+    VIDEO_1080 = '1080P',
+    VIDEO_4K = '4K',
+    VIDEO_2K = '2K',
+}
+
+export default ContentQuality;
