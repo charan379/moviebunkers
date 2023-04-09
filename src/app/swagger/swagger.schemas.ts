5dcfd6fd21df2d9292cfa1ca5be49c2f674f9632
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-09 15:13:57 +0530

    `new` newLink swagger schema

--- src/app/swagger/swagger.schemas.ts
+++ src/app/swagger/swagger.schemas.ts
@@ -1,14 +1,16 @@
+import linkSchema from "@joiSchemas/linkSchema";
 import movieSchema from "@joiSchemas/movie.joi.schema";
 import tvSchema from "@joiSchemas/tv.joi.schema";
 import { lgoinSchema, newUserSchema, userUpdateSchema } from "@joiSchemas/user.joi.schemas";
 import joiToSwagger from "joi-to-swagger";
 
-const swaggerSchemas =  {
+const swaggerSchemas = {
     new_user: joiToSwagger(newUserSchema).swagger,
     login: joiToSwagger(lgoinSchema).swagger,
     update_user: joiToSwagger(userUpdateSchema).swagger,
     new_movie: joiToSwagger(movieSchema).swagger,
     new_tv: joiToSwagger(tvSchema).swagger,
+    new_link: joiToSwagger(linkSchema).swagger,
 }
 
 export default swaggerSchemas;
