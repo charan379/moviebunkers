ac9844681287e7f2670a958774dc86cc54a4a2d5
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-10 13:19:41 +0530

    `update` link model updated

--- src/app/models/links.model.ts
+++ src/app/models/links.model.ts
@@ -4,50 +4,54 @@ import ContentType from "@constants/contentTypes.enum";
 import LinkType from "@constants/linkTypes.enum";
 import ContentQuality from "@constants/contenetQuality.enum";
 
-
 const linkSchema: Schema<ILink> = new Schema<ILink>(
-    {
-        parentId: {
-            type: Schema.Types.ObjectId,
-        },
-        contentType: {
-            type: String,
-            enum: [...Object.values(ContentType)],
-            required: [true, "content type required"],
-        },
-        languages: {
-            type: [Object],
-            _id: false,
-        },
-        linkType: {
-            type: String,
-            enum: [...Object.values(LinkType)],
-            required: [true, 'link type is required']
-        },
-        quality: {
-            type: [String],
-            enum: [...Object.values(ContentQuality)],
-            required: [true, 'atleast one quality type is required']
-        },
-        link: {
-            type: String,
-            required: [true, 'link is required']
-        },
-        title: {
-            type: String,
-            required: [true, 'title is required']
-        },
-        remarks: {
-            type: String
-        }
+  {
+    _id: {
+      type: Schema.Types.ObjectId,
+      index: true,
     },
-    {
-        timestamps: {
-            createdAt: true,
-            updatedAt: true,
-        },
-        collection: "links"
-    }
+    parentId: {
+      type: Schema.Types.ObjectId,
+      index: true,
+    },
+    contentType: {
+      type: String,
+      enum: Object.values(ContentType),
+      required: [true, "content type required"],
+    },
+    languages: {
+      type: [Object],
+      _id: false,
+    },
+    linkType: {
+      type: String,
+      enum: Object.values(LinkType),
+      required: [true, "link type is required"],
+    },
+    quality: {
+      type: [String],
+      enum: Object.values(ContentQuality),
+      required: [true, "atleast one quality type is required"],
+    },
+    link: {
+      type: String,
+      required: [true, "link is required"],
+    },
+    title: {
+      type: String,
+      required: [true, "title is required"],
+    },
+    remarks: {
+      type: String,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+    collection: "links",
+  }
 );
 
 const LinkModel: Model<ILink> = mongoose.model<ILink>("Link", linkSchema);
