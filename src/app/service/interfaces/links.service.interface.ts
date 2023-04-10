c1f3a45c05743762bac859665cbb6b228cc551bc
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-10 14:58:20 +0530

    `update` link service interface updated

--- src/app/service/interfaces/links.service.interface.ts
+++ src/app/service/interfaces/links.service.interface.ts
@@ -1,12 +1,11 @@
 import LinkDTO from "@dto/link.dto";
 import ILink from "@models/interfaces/ILinks";
 
-
 interface ILinksService {
-    create(linkDto: Partial<ILink>): Promise<LinkDTO>;
-    getAllByParentId(parentId: string): Promise<LinkDTO[]>;
-    deleteById(id: string): Promise<void>;
-    updateById(id: string): Promise<LinkDTO>;
+  create(linkDto: Partial<ILink>): Promise<LinkDTO>;
+  getLinksByParentId(parentId: string): Promise<LinkDTO[]>;
+  deleteById(id: string): Promise<void>;
+  updateById(id: string, update: Partial<ILink>): Promise<LinkDTO>;
 }
 
 export default ILinksService;
