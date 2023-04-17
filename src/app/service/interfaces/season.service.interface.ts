635078b3952569d7667f1f37dca507ccdd5a375c
charan379 <37008444+charan379@users.noreply.github.com>
2023-04-17 20:53:02 +0530

    `update` renamed as per updateSeasonById naming convention

--- src/app/service/interfaces/season.service.interface.ts
+++ src/app/service/interfaces/season.service.interface.ts
@@ -6,7 +6,7 @@ import { ISeason } from "@models/interfaces/season.interface";
 interface ISeasonService {
     create(season: Partial<ISeason>): Promise<SeasonDTO>;
     getSeasonsByTvShowId(tvShowId: string): Promise<SeasonDTO[]>
-    updateSeasonByid(id: string, update: Partial<ISeason>): Promise<SeasonDTO>;
+    updateSeasonById(id: string, update: Partial<ISeason>): Promise<SeasonDTO>;
     deleteSeasonById(id: string): Promise<void>;
     deleteAllSeasonByTVShowId(tvShowId: string): Promise<void>;
 }
