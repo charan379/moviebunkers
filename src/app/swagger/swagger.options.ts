65373c167355ccf6a2ef6b505ed7da27aa700357
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-18 15:15:20 +0530

    version `2.2.1`

--- src/app/swagger/swagger.options.ts
+++ src/app/swagger/swagger.options.ts
@@ -9,7 +9,7 @@ export const swaggerOptions = {
     info: {
       title: "moviebunkers",
       description: "moviebunkers backend server",
-      version: "2.2.0",
+      version: "2.2.1",
       contact: {
         name: "charan379",
         url: "#",
