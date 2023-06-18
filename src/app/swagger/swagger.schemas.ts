f744f9464437c5f1844b876191d3d65722d82946
charan379 <37008444+charan379@users.noreply.github.com>
2023-06-18 19:49:19 +0530

    new schemas added

--- src/app/swagger/swagger.schemas.ts
+++ src/app/swagger/swagger.schemas.ts
@@ -2,17 +2,18 @@ import { episodeSchema } from "@joiSchemas/episode.joi.schema";
 import linkSchema from "@joiSchemas/linkSchema";
 import { seasonSchema } from "@joiSchemas/season.joi.schema";
 import titleSchema from "@joiSchemas/title.joi.schema";
-import { lgoinSchema, newUserSchema, userUpdateSchema } from "@joiSchemas/user.joi.schemas";
+import { loginSchema, msAdmUpdatePassSchema, newUserSchema, userUpdateSchema } from "@joiSchemas/user.joi.schemas";
 import joiToSwagger from "joi-to-swagger";
 
 const swaggerSchemas = {
     new_user: joiToSwagger(newUserSchema).swagger,
-    login: joiToSwagger(lgoinSchema).swagger,
+    login: joiToSwagger(loginSchema).swagger,
     update_user: joiToSwagger(userUpdateSchema).swagger,
     title: joiToSwagger(titleSchema).swagger,
     link: joiToSwagger(linkSchema).swagger,
     episode: joiToSwagger(episodeSchema).swagger,
     season: joiToSwagger(seasonSchema).swagger,
+    ms_adm_update_pass: joiToSwagger(msAdmUpdatePassSchema).swagger,
 }
 
 export default swaggerSchemas;
